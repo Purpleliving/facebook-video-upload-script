72d58cfb13253de3dbd609bdd42ded3ded0342a7
Purpleliving <142207135+Purpleliving@users.noreply.github.com>
2024-10-28 15:31:39 +0100

    Create config.sh

---
+++ config.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Configuration for Facebook API
+PAGE_ID="your_facebook_page_id"  # Replace with your Facebook Page ID
+ACCESS_TOKEN="your_page_access_token"  # Replace with your Facebook Page Access Token
